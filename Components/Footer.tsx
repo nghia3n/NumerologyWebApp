41e43209f585fe13514f582bd5f9ec24aa50a132
nghia3n <134572006+nghia3n@users.noreply.github.com>
2023-05-25 13:01:50 +0700

    Update Footer.tsx

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -12,12 +12,12 @@ export const Footer: FC = () => {
             <Tooltip label={colorMode + " mode"}>
                <CustomCard>
                   <Image
-                     src="https://upanh.svvui.com/file/20230525054536-Honu-02.png"
+                     src="/Images/numerologyPNG.png"
                      alt="numerology logo"
                      width="50%"
                      height="50%"
                      placeholder="blur"
-                     blurDataURL="https://upanh.svvui.com/file/20230525054536-Honu-02.png"
+                     blurDataURL="/Images/numerologyPNG.png"
                      style={{
                         cursor: "pointer",
                      }}
