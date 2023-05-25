f7fab15f512e50d7467dfa7c0e452c8f8820a1f0
nghia3n <134572006+nghia3n@users.noreply.github.com>
2023-05-25 12:51:41 +0700

    Update Header.tsx

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -5,8 +5,8 @@ export const Header: FC = () => {
    return (
       <Head>
          <title>Honu.vn - Thần số học & Tử vi</title>
-         <meta name="description" content="Honu.vn - Thần số học & Tử vi" />
-         <link rel="icon" href="/Images/numerologyPNG.png" />
+         <meta name="description" content="Honu.vn - Tra cứu Chỉ số Của bạn" />
+         <link rel="icon" href="https://upanh.svvui.com/file/20230525054536-Honu-02.png" />
       </Head>
    );
 };
